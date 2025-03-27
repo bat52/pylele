152b0273f47af53ee0ab9dd292c8991ac0d99e49
Marco Merlin <marcomerli+github@gmail.com>
2025-03-28 00:59:38 +0100

    add carved hex option to worm key

--- src/pylele/pylele2/worm_key.py
+++ src/pylele/pylele2/worm_key.py
@@ -4,6 +4,8 @@
     Pylele Worm Key
 """
 
+import argparse
+
 import os
 import sys
 
@@ -13,11 +15,28 @@ from pylele.config_common import TunerType, WormConfig
 from b13d.api.constants import FIT_TOL
 from b13d.api.solid import main_maker, test_loop
 from b13d.api.core import Shape, Implementation
-from pylele.pylele2.base import LeleBase
+from pylele.pylele2.base import LeleBase, pylele_base_parser
 from b13d.parts.rounded_box import RoundedBox
+
+def pylele_worm_key_parser(parser=None):
+    """
+    Pylele Worm Key Parser
+    """
+    if parser is None:
+        parser = argparse.ArgumentParser(description="Pylele Worm Key parser")
+
+    parser.add_argument("-wkch", "--worm_key_carved_hex",
+                        help="Carved hex hole instead of 3d print hex shaft in worm key", 
+                        action="store_true")
+
+    return parser
 
 class LeleWormKey(LeleBase):
     """Pylele Worm Key Generator class"""
+
+    def gen_parser(self, parser=None):
+        parser = super().gen_parser(parser=parser)
+        return pylele_worm_key_parser(parser=parser)
 
     def gen(self) -> Shape:
         """Generate Worm Key"""
@@ -37,7 +56,17 @@ class LeleWormKey(LeleBase):
         kyLen = wcfg.buttonKeyLen + 2 * cutAdj
         gapAdj = wcfg.gapAdj
 
-        key = self.api.regpoly_extrusion_x(kyLen, kyRad, 6).mv(joinTol -kyLen/2 -kbHt -btnHt, 0, 0)
+        if self.cli.worm_key_carved_hex:
+            kyRad += 2 * cutAdj
+            kyLen += btnHt
+
+        key = self.api.regpoly_extrusion_x(kyLen, kyRad, 6)
+        
+        if self.cli.worm_key_carved_hex:
+            key <<= (-btnHt, 0, 0)
+        else:
+            key <<= (joinTol -kyLen/2 -kbHt -btnHt, 0, 0)
+
         base = self.api.regpoly_extrusion_x(kbHt, kbRad, 36) if isBlender else self.api.cylinder_x(kbHt, kbRad)
         base = base.mv(-kbHt/2 -btnHt, 0, 0)
 
@@ -56,7 +85,15 @@ class LeleWormKey(LeleBase):
                 .mv(-btnHt/2, 0, 0)
             btn = box.gen_full()
 
-        btn += base + key
+        btn += base
+
+        if self.cli.worm_key_carved_hex:
+            btn -= key
+            # screw hole
+            btn -= self.api.cylinder_x(100, 1)
+        else:
+            btn += key
+
         maxTnrY = max([y for _, y, _ in txyzs])
         btn = btn.mv(tailX - joinTol, maxTnrY + btnTck + gapAdj/2, -1 -btnWth/2)
         return btn
@@ -70,17 +107,17 @@ def main(args=None):
 def test_worm_key(self, apis=None):
     """Test Worm Key"""
     tests = {
-        "cut": ["-t", "worm", "-C"],
-        "big_worm": ["-t", "bigWorm"],
+        "cut": ["-t", TunerType.WORM.name, "-C"],
+        "big_worm": ["-t", TunerType.BIGWORM.name],
+        "carved" : ["-t", TunerType.WORM.name, "-wkch"],
     }
     test_loop(module=__name__, tests=tests, apis=apis)
 
 
 def test_worm_key_mock(self):
     """Test Worm Key"""
-    test_worm_key(self, apis=["mock"])
-
+    test_worm_key(self, apis=[Implementation.MOCK])
 
 if __name__ == "__main__":
-    main(sys.argv[1:]+["-t","worm"])
+    main(sys.argv[1:]+["-t",TunerType.WORM.name])
 
